6e74c62556a1255ccfc5ea4712da1c09b6f031fc
KHIZRA-IQBAL <iqbalkhizra026@gmail.com>
2026-03-01 16:19:16 +0500

    some changes

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        // Aapka updated link password ke sath
-        const atlasURL = "mongodb+srv://admin:admin123@ecommerce-project.4gpynmv.mongodb.net/ecommerce_db?retryWrites=true&w=majority&appName=Ecommerce-Project";
+        // Ab hum variable ki jagah process.env use karenge
+        const atlasURL = process.env.MONGO_URI; 
         
         await mongoose.connect(atlasURL);
         console.log("Fresh MongoDB Atlas Connected... ☁️✅");
